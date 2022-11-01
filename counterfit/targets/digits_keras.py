8ef40094409d3f17420e7839d509184725e3a5df
gary <dlmgary@outlook.com>
2022-11-01 18:59:30 -0400

    style: update digits keras formatting

--- counterfit/targets/digits_keras.py
+++ counterfit/targets/digits_keras.py
@@ -31,15 +31,12 @@ class DigitKeras(CFTarget):
 
     def create_model(self):
         # 0. get started
-        print(
-            f"    - Training new model. Begin MNIST using Keras {K.__version__}")
+        print(f"    - Training new model. Begin MNIST using Keras {K.__version__}")
         np.random.seed(1)
         tf.random.set_seed(1)
-
         # 1. load data
         print("     - Loading train and test data ")
-        (train_x, train_y), \
-            (test_x, test_y) = K.datasets.mnist.load_data()
+        (train_x, train_y), (test_x, test_y) = K.datasets.mnist.load_data()
         train_x = train_x.reshape(60_000, 28, 28, 1)
         test_x = test_x.reshape(10_000, 28, 28, 1)
         train_x = train_x.astype(np.float32)
@@ -48,78 +45,38 @@ class DigitKeras(CFTarget):
         test_x /= 255
         train_y = K.utils.to_categorical(train_y, 10)
         test_y = K.utils.to_categorical(test_y, 10)
-
         # self.X = [test_x]
         self.test_x = test_x
-
         # 2. define model
-        print(
-            "   - Creating network with two Convolution, two Dropout, two Dense layers ")
+        print("   - Creating network with two Convolution, two Dropout, two Dense layers ")
         g_init = K.initializers.glorot_uniform(seed=1)
         opt = K.optimizers.Adam(learning_rate=0.01)
         x = K.layers.Input(shape=(28, 28, 1))
-        con1 = K.layers.Conv2D(
-            filters=32,
-            kernel_size=(3, 3),
-            kernel_initializer=g_init,
-            activation='relu',
-            padding='valid')(x)
-
-        con2 = K.layers.Conv2D(
-            filters=64,
-            kernel_size=(3, 3),
-            kernel_initializer=g_init,
-            activation='relu',
-            padding='valid')(con1)
-
+        con1 = K.layers.Conv2D(filters=32, kernel_size=(3, 3), kernel_initializer=g_init, activation='relu', padding='valid')(x)
+        con2 = K.layers.Conv2D(filters=64, kernel_size=(3, 3), kernel_initializer=g_init, activation='relu', padding='valid')(con1)
         mp1 = K.layers.MaxPooling2D(pool_size=(2, 2))(con2)
         do1 = K.layers.Dropout(0.25)(mp1)
         z = K.layers.Flatten()(do1)
-        fc1 = K.layers.Dense(
-            units=128,
-            kernel_initializer=g_init,
-            activation='relu')(z)
-
+        fc1 = K.layers.Dense(units=128, kernel_initializer=g_init, activation='relu')(z)
         do2 = K.layers.Dropout(0.5)(fc1)
-        fc2 = K.layers.Dense(
-            units=10,
-            kernel_initializer=g_init,
-            activation='softmax')(do2)
-
+        fc2 = K.layers.Dense(units=10, kernel_initializer=g_init, activation='softmax')(do2)
         model = K.models.Model(x, fc2)
-        model.compile(
-            loss='categorical_crossentropy',
-            optimizer=opt,
-            metrics=['accuracy'],
-            run_eagerly=False)
-
+        model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'], run_eagerly=False)
         # 3. train model
         batch_size = 100
         max_epochs = 2
         print(f"    - Starting training with batch size {batch_size}")
-        model.fit(
-            train_x,
-            train_y,
-            batch_size=batch_size,
-            epochs=max_epochs,
-            verbose=1)
+        model.fit(train_x, train_y, batch_size=batch_size, epochs=max_epochs, verbose=1)
         print("     - Training finished ")
-
         # 4. evaluate model
-        evaluation = model.evaluate(
-            test_x,
-            test_y,
-            verbose=0)
-
+        evaluation = model.evaluate(test_x, test_y, verbose=0)
         loss = evaluation[0]
         acc = evaluation[1] * 100
         print(f"    - Test data: loss = {loss}, accuracy = {acc}")
-
         # 5. save model
         print("     - Saving MNIST model to disk ")
         model.save(self.fullpath("mnist_model.h5"))
         self.model = model
-
         print("[+] Done!")
 
     def predict(self, x):
