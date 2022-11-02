47f2645b891acc8a3c9e1e1884d2d2f9eef14a45
gary <dlmgary@outlook.com>
2022-11-01 20:06:25 -0400

    refactor: update initializers in logger.py

--- counterfit/core/logger.py
+++ counterfit/core/logger.py
@@ -4,29 +4,30 @@ import orjson
 
 
 class CFLogger:
-    """Base class for all loggers.
-
-    Returns:
-        [type]: [description]
+    """ Base class for all loggers.
     """
     num_queries: int
     
     def __init__(self) -> None:
         pass
 
+
 class BasicLogger(CFLogger):
-    """The default logger. Only logs the number of queries against a model.
+    """ The default logger. Only logs the number of queries against a model.
     """
     def __init__(self, **kwargs):
+        super(CFLogger).__init__()
         self.num_queries = 0
 
     def log(self, item):
         self.num_queries += 1
 
+
 class JSONLogger(CFLogger):
     """Logs queries to a json file saved to disk.
     """
     def __init__(self, **kwargs):
+        super(CFLogger).__init__()
         attack_id = kwargs["attack_id"]
         ts = kwargs['ts']
         self.num_queries = 0
@@ -42,17 +43,9 @@ class JSONLogger(CFLogger):
         self.logs.append(item)
         self.num_queries += 1
 
+
 def get_attack_logger_obj(logger_type: str) -> CFLogger:
-    """Factory method to get the requested logger.
-
-    Args:
-        logger_type ([type]): [description]
-
-    Raises:
-        KeyError: [description]
-
-    Returns:
-        [type]: [description]
+    """ Factory method to get the requested logger.
     """
 
     attack_logger_obj_map = {
