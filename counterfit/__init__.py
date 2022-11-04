3f3b18e91adb20e50078db22d51d8ef57f113ec2
gary <dlmgary@outlook.com>
2022-11-04 12:04:05 -0400

    refactor: fix import warnings.

--- counterfit/__init__.py
+++ counterfit/__init__.py
@@ -1,3 +1,11 @@
+import os
+import warnings
+
+# make tensorflow quiet
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+warnings.filterwarnings('ignore')
+
 from . import core, data, frameworks, reporting, targets
 from .core.attacks import CFAttack
 from .core.core import Counterfit
@@ -7,13 +15,6 @@ from .core.options import CFOptions
 from .core.output import CFPrint
 from .core.targets import CFTarget
 
-import os
-import warnings
-
-# make tensorflow quiet
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-warnings.filterwarnings('ignore')
 
 __version__ = '1.1.0'
 name = 'counterfit'
